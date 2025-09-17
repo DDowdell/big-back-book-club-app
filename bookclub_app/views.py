b181b86641c74a4c6d1bc068d21e048112d02a78
Dae Dowdell <dow_dell2010@yahoo.com>
2025-09-17 13:45:35 -0400

    book details page

--- bookclub_app/views.py
+++ bookclub_app/views.py
@@ -12,44 +12,6 @@ def book_index(request):
     books = Book.objects.all()
     return render(request, 'books/index.html', {'books': books})
 
-
-# class Book:
-#     def __init__(self, title, author, description, genre):
-#         self.title = title
-#         self.author = author
-#         self.description = description
-#         self.genre = genre
-
-
-# books = [
-#     Book(
-#       'Harry Potter Series', 
-#       'J.K. Rowling', 
-#       'The boy who lived', 
-#       'Fantasy',
-#     ),
-#     Book(
-#       'The Dark Tower Series', 
-#       'Stephen King', 
-#       'A gunslingers quest', 
-#       'Horror',
-#     ),
-#     Book(
-#         'The Duke and I: Bridgerton Series',
-#         'Julia Quinn',
-#         'Daphne loves Simon?',
-#         'Romance',
-#     ),
-#     Book(
-#         'The Wheel of Time Series',
-#         'Robert Jordan',
-#         'Save the world from the Dark One',
-#         'Sci-Fi',
-#     ),
-#     Book(
-#         'Morder is Forever Series',
-#         'James Patterson',
-#         'Murder and Investigation',
-#         'True Crime',
-#     ),
-# ]
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/detail.html', {'book': book})
