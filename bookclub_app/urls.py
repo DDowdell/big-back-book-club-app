d121328d1a7bb946a7ac331ecba6b5259dd358c8
Dae Dowdell <dow_dell2010@yahoo.com>
2025-09-19 17:07:31 -0400

    delete comment and undo suggestion buttons

--- bookclub_app/urls.py
+++ bookclub_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .views import delete_comment, undo_suggestion
+
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
@@ -12,5 +14,13 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('books/<int:book_id>/add_comment/', views.add_comment, name='add_comment'),
     path('books/suggest/', views.suggest_book, name='suggest_book'),
-    path('book_suggestion/<int:suggestion_id>/vote/', views.vote_for_suggestion, name='vote_for_suggestion'),
+    path(
+        'book_suggestion/<int:suggestion_id>/vote/',
+        views.vote_for_suggestion,
+        name='vote_for_suggestion',
+    ),
+    path('comment/delete/<int:comment_id>/', delete_comment, name='delete_comment'),
+    path(
+        'suggestion/undo/<int:suggestion_id>/', undo_suggestion, name='undo_suggestion'
+    ),
 ]
--- bookclub_app/views.py
+++ bookclub_app/views.py
@@ -10,32 +10,36 @@ from django.http import Http404
 
 
 class Home(LoginView):
-    template_name = 'home.html'
+    template_name = "home.html"
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
 
 
 @login_required
 def book_index(request):
     books = Book.objects.filter(user=request.user)
-    return render(request, 'books/index.html', {'books': books})
+    return render(request, "books/index.html", {"books": books})
 
 
 @login_required
 def book_detail(request, book_id):
     book = Book.objects.filter(id=book_id).first()
     if book is None:
-      raise Http404('Book not found')
+        raise Http404("Book not found")
     comments = Comment.objects.filter(book=book)
     suggestions = BookSuggestion.objects.filter(suggested_book=book)
-    return render(request, 'books/detail.html', {'book': book, 'comments': comments, 'suggestions': suggestions})
+    return render(
+        request,
+        "books/detail.html",
+        {"book": book, "comments": comments, "suggestions": suggestions},
+    )
 
 
 class BookCreate(LoginRequiredMixin, CreateView):
     model = Book
-    fields = ['title', 'author', 'description', 'genre', 'is_current']
+    fields = ["title", "author", "description", "genre", "is_current"]
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -44,27 +48,27 @@ class BookCreate(LoginRequiredMixin, CreateView):
 
 class BookUpdate(LoginRequiredMixin, UpdateView):
     model = Book
-    fields = ['author', 'description', 'genre', 'is_current']
+    fields = ["author", "description", "genre", "is_current"]
 
 
 class BookDelete(LoginRequiredMixin, DeleteView):
     model = Book
-    success_url = '/books/'
+    success_url = "/books/"
 
 
 def signup(request):
-    error_message = ''
-    if request.method == 'POST':
+    error_message = ""
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('book-index')
+            return redirect("book-index")
         else:
-            error_message = 'Invalid sign up - try again'
+            error_message = "Invalid sign up - try again"
     form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message}
-    return render(request, 'signup.html', context)
+    context = {"form": form, "error_message": error_message}
+    return render(request, "signup.html", context)
 
 
 @login_required
@@ -73,17 +77,43 @@ def add_comment(request, book_id):
         book = Book.objects.filter(id=book_id).first()
         if book is None:
             raise Http404("Book not found")
-        
-        content = request.POST['content']
+
+        content = request.POST["content"]
         Comment.objects.create(book=book, user=request.user, content=content)
-    return redirect('book-detail', book_id=book_id)
+    return redirect("book-detail", book_id=book_id)
+
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = Comment.objects.filter(id=comment_id, user=request.user).first()
+    if comment:
+        comment.delete()
+
+    return redirect("book-detail", book_id=comment.book.id if comment else None)
+
 
 @login_required
 def suggest_book(request):
     if request.method == "POST":
-        suggested_book_id = request.POST['book_id']  # This should come from your form
-        BookSuggestion.objects.create(user=request.user, suggested_book_id=suggested_book_id)
-        return redirect('book-detail', book_id=suggested_book_id)
+        suggested_book_id = request.POST["book_id"]
+        BookSuggestion.objects.create(
+            user=request.user, suggested_book_id=suggested_book_id
+        )
+        return redirect("book-detail", book_id=suggested_book_id)
+
+
+@login_required
+def undo_suggestion(request, suggestion_id):
+    suggestion = BookSuggestion.objects.filter(
+        id=suggestion_id, user=request.user
+    ).first()
+    if suggestion:
+        suggestion.delete()
+
+    return redirect(
+        "book-detail", book_id=suggestion.suggested_book.id if suggestion else None
+    )
+
 
 @login_required
 def vote_for_suggestion(request, suggestion_id):
@@ -93,4 +123,4 @@ def vote_for_suggestion(request, suggestion_id):
             raise Http404("Suggestion not found")
 
         Vote.objects.create(user=request.user, suggested_book=suggestion)
-        return redirect('book-detail', book_id=suggestion.suggested_book.id)
+        return redirect("book-detail", book_id=suggestion.suggested_book.id)
